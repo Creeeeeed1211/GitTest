7bc12d46194c834f2fbbba4985ffed81e47f6696
Creed <bploiolqd@gmail.com>
2024-10-18 10:08:59 +0100

    2nd surface

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@ int main()
     std::cout<<"Hello World"<<std::endl;
     std::cout<<"1st change on github and surface"<<std::endl;
     std::cout<<"1st change on linux"<<std::endl;
+    std::cout<<"2nd change on surface"<<std::endl;
+    
 
     return 0;
 }
