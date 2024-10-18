006631c35214404ca91bc56c698749bf0ddb65ae
Sicheng Chen [sc22s3c] <sc22s3c@uol-pc-051699.leeds.ac.uk>
2024-10-18 10:02:28 +0100

    linux commit

--- main.cpp
+++ main.cpp
@@ -4,5 +4,7 @@ int main()
 {
     std::cout<<"Hello World"<<std::endl;
     std::cout<<"1st change on github and surface"<<std::endl;
+    std::cout<<"1st change on linux"<<std::endl;
+
     return 0;
 }
